0e58c2cebb3e34f434e4baeb14d90bdca60218dc
EriickW <werick631@gmail.com>
2022-04-07 17:27:09 -0300

    .

--- Desafios/Área da Circunferencia/main.js
+++ Desafios/Área da Circunferencia/main.js
@@ -1,6 +1,6 @@
 //Desafio de programa para mosrtar a area de uma circunferencia 
 
-const pi = 3.14;
+const PI = 3.14;
 let raio = 5;
 let area = pi * raio**2;
-console.log("O valor da area da circunferencia é " + area +" !")
+console.log("O valor da area da circunferencia é " + area +" !");
