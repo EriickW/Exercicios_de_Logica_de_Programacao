b50f858dbe2fdffe238d7de8bfc49da998100b19
EriickW <werick631@gmail.com>
2022-04-12 21:07:00 -0300

    .

---
+++ Variaveis/ex001.js
@@ -0,0 +1,10 @@
+/*Exercício 1.1
+Faça um programa que exiba 3 números na tela.
+
+Dica - exibindo (ou "imprimindo") números na tela:*/
+let n1 = 30;
+let n2 = 10;
+let n3 = -3;
+console.log(n1)
+console.log(n2)
+console.log(n3)
---
+++ Variaveis/ex002.js
@@ -0,0 +1,5 @@
+//Faça um programa que leia 3 números, some-os e exiba a média entre eles.
+
+let a=2, b=3, c=4;
+let media = (a+b+c)/3;
+console.log(media)
