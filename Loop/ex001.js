971b685f212ee698540309ef6260296a86d570fd
EriickW <werick631@gmail.com>
2022-04-16 17:52:23 -0300

    .

---
+++ Loop/ex001.js
@@ -0,0 +1,8 @@
+//Escreva um programa que calcule e mostre a tabuada (até 10x) de um número qualquer, digitado pelo usuário.
+ 
+let numeroEscolhido = 7;
+for(i=0;i<=10;i++){
+    let tabuada = 7*i
+    console.log(`${numeroEscolhido} x ${i} = ${tabuada}`)
+
+}
